6562f484fce0f3cad29729e7df630dd8650d240e
VuHuu <162543040+Vucore@users.noreply.github.com>
2025-03-16 23:07:41 +0700

    update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import json
 import random
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import TfidfVectorizer
+import streamlit as st
 
 tags = []
 patterns = []
@@ -30,5 +31,28 @@ def ChatBot(input_message):
             responses = random.choice(intent['responses'])
             return responses
 
-input_message = input('Enter: ')
-print(ChatBot(input_message))
+# App
+st.title('Mental Health ChatBot AI')
+
+#Init chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# display chat messages from history
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# react to user input
+if prompt := st.chat_input("What is up?"):
+    # display user message
+    st.chat_message("user").markdown(prompt)
+    # add user message to chat history
+    st.session_state.messages.append({"role" : "user", "content" : prompt})
+
+    response = f"AI ChatBot: " + ChatBot(prompt)
+    # display assistant response
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    # add assistant reponse
+    st.session_state.messages.append({"role": "assistant", "content": response})
